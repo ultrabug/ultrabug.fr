32e1cf824ed639983fa7f424261d80a1a83488a1
Ultrabug <ultrabug@gentoo.org>
2021-08-24 12:14:49 +0200

    migrate to dynamic tech blog redirections on build

--- gen-redirects.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from os import chdir
-from pathlib import Path
-
-chdir("docs/")
-blog = Path("Tech Blog")
-for md in sorted(blog.glob('**/*.md'), reverse=True):
-    print(f'        "{md.name[11:]}": "{md}"')
